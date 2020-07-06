e73a18808d851ea1d0f042efccb78959acf43723
marvinkabiru <marvinkabiru@gmail.com>
2020-07-06 14:38:02 +0300

    jsday1

---
+++ main.js
@@ -0,0 +1,11 @@
+// document.getElementById("button").onclick = function(){
+//     document.getElementById("confirm").innerHTML = "Order placed. check email for confirmation.";
+//     document.getElementById("button").style.display = "none";
+// }
+
+// variables
+var username = prompt();
+alert(username);
+
+var age = 50;
+console.log(age);
